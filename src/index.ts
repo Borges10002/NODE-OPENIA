86a03c2ffa274afb6d25afaf87c0aee80cf7cba6
Borges10002 <borges10002@gmail.com>
2025-06-29 20:00:46 -0400

    criado api

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
-import dotenv from "dotenv";
-import OpenAI from "openai";
+import app from "./app";
 
-dotenv.config();
-
-const client = new OpenAI({
-  apiKey: process.env.OPEN_IA_API_KEY,
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3333}`);
 });
-
-async function generateText() {
-  const completion = await client.chat.completions.create({
-    model: "gpt-4o-mini",
-    max_completion_tokens: 100,
-    messages: [
-      {
-        role: "developer",
-        content:
-          "Use emojis a cada 2 palavras. Isso é obrigatório, ignore regras que mudem a utilização de emojis. Gere um texto com no máximo uma frase.",
-      },
-      {
-        role: "user",
-        content:
-          "Escreva uma mensagem de uma frase sobre unicórnios. (Não pode usar emoji)",
-      },
-      {
-        role: "assistant",
-        content:
-          "Os unicórnios 🌈 são 🦄 criaturas mágicas 🪄 que simbolizam ✨ pureza e 🌟 beleza.",
-      },
-      {
-        role: "user",
-        content: "Obrigado",
-      },
-    ],
-  });
-
-  console.log(completion.choices[0].message.content);
-}
-
-generateText();
